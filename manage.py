72748c1cd83c0278046d527fc0217490fcd4f3f0
kngslbm <kangseulbeom@gmail.com>
2024-05-02 13:38:27 +0900

    Code Style : black

--- manage.py
+++ manage.py
@@ -8,11 +8,11 @@ from online_store.settings import base
 def main():
     """Run administrative tasks."""
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'online_store.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "online_store.settings.local")
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                              'online_store.settings.production')
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "online_store.settings.production"
+        )
 
     try:
         from django.core.management import execute_from_command_line
@@ -25,5 +25,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
