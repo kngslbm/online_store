fcb5f55ba98e78169df9ebc17f8d5be8e5e77118
kngslbm <kangseulbeom@gmail.com>
2024-05-02 17:53:48 +0900

    views.py 작성

--- online_store/product/views.py
+++ online_store/product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import CategoreySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all categoreys.
+    """
+
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        serializer = CategoreySerializer(self.queryset, many=True)
+        return Response(serializer.data)
